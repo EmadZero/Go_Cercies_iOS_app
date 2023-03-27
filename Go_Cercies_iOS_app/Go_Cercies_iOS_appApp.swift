5cdd2f78ce504bf84cf0a2aa7c84335e46246154
Emad Aljabry <ealjabry@gmail.com>
2023-03-27 11:50:43 -0400

    Initial Commit

---
+++ Go_Cercies_iOS_app/Go_Cercies_iOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Go_Cercies_iOS_appApp.swift
+//  Go_Cercies_iOS_app
+//
+//  Created by Emad A on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Go_Cercies_iOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
